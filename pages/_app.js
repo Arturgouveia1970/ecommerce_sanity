48708fa624c1fbd606b847a478cad0710fbb545e
Artur <artgouveiag@gmail.com>
2023-11-11 13:37:07 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import React from 'react';
+import '../styles/globals.css';
+import { Layout } from '@/components';
+import { StateContext } from '@/context/StateContext';
+import { Toast } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
+    
+    
+
